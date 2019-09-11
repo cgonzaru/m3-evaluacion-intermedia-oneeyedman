064bbfb3b87c725ce1306f5e7345e23bc434efba
Carlos Mañas <carlos@spacenomads.com>
2019-09-11 12:09:02 +0200

    Add fetch solution

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import PokeList from './components/PokeList';
 
 import './App.css';
 
+const ENDPOINT = 'https://raw.githubusercontent.com/Adalab/m3-evaluacion-intermedia-oneeyedman/componentbranch/pokemons.json';
 
 
 class App extends React.Component {
@@ -10,80 +11,21 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      data: [
-              {
-                "id": 1,
-                "name": "bulbasaur",
-                "types": ["poison", "grass"],
-                "evolution": null,
-                "url": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
-              },
-              {
-                "id": 2,
-                "name": "ivysaur",
-                "types": ["poison", "grass"],
-                "evolution": "bulbasaur",
-                "url": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png"
-              },
-              {
-                "id": 3,
-                "name": "venusaur",
-                "types": ["poison", "grass"],
-                "evolution": "ivysaur",
-                "url": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png"
-              },
-              {
-                "id": 4,
-                "name": "charmander",
-                "types": ["fire"],
-                "evolution": null,
-                "url": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"
-              },
-              {
-                "id": 5,
-                "name": "charmeleon",
-                "types": ["fire"],
-                "evolution": "charmander",
-                "url": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png"
-              },
-              {
-                "id": 6,
-                "name": "charizard",
-                "types": ["flying", "fire"],
-                "evolution": "charmeleon",
-                "url": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png"
-              },
-              {
-                "id": 7,
-                "name": "squirtle",
-                "types": ["water"],
-                "evolution": null,
-                "url": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png"
-              },
-              {
-                "id": 8,
-                "name": "wartortle",
-                "types": ["water"],
-                "evolution": "squirtle",
-                "url": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png"
-              },
-              {
-                "id": 9,
-                "name": "blastoise",
-                "types": ["water"],
-                "evolution": "wartortle",
-                "url": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png"
-              },
-              {
-                "id": 10,
-                "name": "caterpie",
-                "types": ["bug"],
-                "evolution": null,
-                "url": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png"
-              }
-            ]
-
+      data: []
     };
+
+    this.handlePokemon();
+  }
+
+  handlePokemon() {
+    fetch(ENDPOINT)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          data: data
+        });
+      });
+    
   }
 
   render() {
